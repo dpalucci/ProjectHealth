25c57d70506068f9c3895bfe27716f3718d98b73
Domenic Palucci <dpalucci@hotmail.com>
2016-09-07 13:26:09 -0400

    connection string

---
+++ ProjectHealth/Database.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectHealth
+{
+    class Database
+    {
+        Data Source=ipd8.database.windows.net;Initial Catalog=ProjectHealth;Integrated Security=False;User ID=ipd8abbott;Password=Abbott2000;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False
+    }
+}
