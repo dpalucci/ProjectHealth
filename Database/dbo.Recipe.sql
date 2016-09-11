3605bd534fb0d3e82a7e72efff01284750eedba9
davide <davidmo2003@hotmail.com>
2016-09-10 23:14:58 -0400

    detail infomation of the recipes inputed to the Table Recipe

---
+++ Database/dbo.Recipe.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Recipe] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [RecipeName] NVARCHAR (50) NOT NULL,
+    [Calories]   FLOAT (53)    NULL,
+    [Fat]        FLOAT (53)    NULL,
+    [Carb]       FLOAT (53)    NULL,
+    [Protein]    FLOAT (53)    NULL,
+    [Type]       NCHAR (10)    NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
