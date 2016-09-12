bbecff3fc6cd78999fb3de351509f4f7b69dcd47
yaohengMo <davidmo2003@hotmail.com>
2016-09-12 12:29:24 -0400

    y

--- ProjectHealth/Database.cs
+++ ProjectHealth/Database.cs
@@ -9,14 +9,12 @@ namespace ProjectHealth
 {
 
     class Database
-<<<<<<< HEAD
+
     {
         const string CONN_STRING = @"Data Source=ipd8vs.database.windows.net;Initial Catalog=ProjectHealthDB;Integrated Security=False;User ID=sqladmin;Password=IPD8rocks!;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-=======
-    {       
-        const string CONN_STRING = @"Data Source = ipd8vs.database.windows.net; Initial Catalog = ProjectHealthDB; Integrated Security = False; User ID = sqladmin; Password=IPD8rocks!;Connect Timeout = 15; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        //const string CONN_STRING = @"Data Source=ipd8.database.windows.net;Initial Catalog=ProjectHealth;Integrated Security=False;User ID=ipd8abbott;Password=Abbott2000;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
->>>>>>> fb7087c259c52dac476feab113ffc6fb75eab77f
+
+          
+       
 
         private SqlConnection conn;
 
