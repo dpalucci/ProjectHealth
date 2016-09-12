1e7827a1eb72d344a8e485bfce73b68aa75bf704
yaohengMo <davidmo2003@hotmail.com>
2016-09-12 12:55:37 -0400

    DBupdate

--- ProjectHealth/Database.cs
+++ ProjectHealth/Database.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 
 namespace ProjectHealth
 {
+    class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public float Calories { get; set; }
+        public float Fat { get; set; }
+        public float Carb { get; set; }
+        public float Protein { get; set; }
+        public string Type { get; set; }
+    }
 
     class Database
 
@@ -22,6 +32,31 @@ namespace ProjectHealth
         {
             conn = new SqlConnection(CONN_STRING);
             conn.Open();
+        }
+
+        public List<Recipe> GetAllRecipes()
+        {
+            List<Recipe> list = new List<Recipe>();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Recipe", conn);
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        int id = reader.GetInt32(reader.GetOrdinal("Id"));
+                        string title = reader.GetString(reader.GetOrdinal("Title"));
+                        float calories = reader.GetFloat(reader.GetOrdinal("Protein"));
+                        float protein = reader.GetFloat(reader.GetOrdinal("Calories"));
+                        float fat = reader.GetFloat(reader.GetOrdinal("Fat"));
+                        Recipe r = new Recipe() { Id = id, Title = title, Protein = protein, Calories = calories, Fat = fat };
+                        list.Add(r);
+                    }
+                }
+            }
+            return list;
+
         }
     }
 }
