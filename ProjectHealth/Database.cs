fb7087c259c52dac476feab113ffc6fb75eab77f
Domenic Palucci <dpalucci@hotmail.com>
2016-09-12 12:11:45 -0400

    update with new connectionDB

--- ProjectHealth/Database.cs
+++ ProjectHealth/Database.cs
@@ -7,20 +7,11 @@ using System.Threading.Tasks;
 
 namespace ProjectHealth
 {
-    // Entity
-    class Recipe
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public float Calories { get; set; }
-        public float Fat { get; set; }
-        public float Carb { get; set; }
-        public float Protein { get; set; }
-        public string Type { get; set; }
-    }
+
     class Database
-    {
-        const string CONN_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Download\ProjectHealthDB.mdf;Integrated Security=True;Connect Timeout=30";
+    {       
+        const string CONN_STRING = @"Data Source = ipd8vs.database.windows.net; Initial Catalog = ProjectHealthDB; Integrated Security = False; User ID = sqladmin; Password=IPD8rocks!;Connect Timeout = 15; Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        //const string CONN_STRING = @"Data Source=ipd8.database.windows.net;Initial Catalog=ProjectHealth;Integrated Security=False;User ID=ipd8abbott;Password=Abbott2000;Connect Timeout=60;Encrypt=False;TrustServerCertificate=False";
 
         private SqlConnection conn;
 
@@ -28,32 +19,6 @@ namespace ProjectHealth
         {
             conn = new SqlConnection(CONN_STRING);
             conn.Open();
-        }
-
-
-        public List<Recipe> GetAllRecipes()
-        {
-            List<Recipe> list = new List<Recipe>();
-
-            SqlCommand cmd = new SqlCommand("SELECT * FROM Recipe", conn);
-            using (SqlDataReader reader = cmd.ExecuteReader())
-            {
-                if (reader.HasRows)
-                {
-                    while (reader.Read())
-                    {
-                        int id = reader.GetInt32(reader.GetOrdinal("Id"));
-                        string title = reader.GetString(reader.GetOrdinal("Title"));
-                        float calories = reader.GetFloat(reader.GetOrdinal("Protein"));
-                        float protein = reader.GetFloat(reader.GetOrdinal("Calories"));
-                        float fat = reader.GetFloat(reader.GetOrdinal("Fat"));
-                        Recipe r = new Recipe() { Id = id, Title = title, Protein = protein, Calories = calories, Fat = fat };
-                        list.Add(r);
-                    }
-                }
-            }
-            return list;
-
         }
     }
 }
